4cbb2f9cba8632c8fa31ebcd115c2d82695ef5d1
rokkuran <lachlanbtaylor@gmail.com>
2019-05-10 15:00:35 +1000

    update to use google api key and store city geocodes

--- world_explored.r
+++ world_explored.r
@@ -1,9 +1,10 @@
-setwd("/home/rokkuran/workspace/maps/")
+# setwd("/home/rokkuran/workspace/maps/")
+setwd("c:/workspace/maps/")
 
-packages <- c('maps', 'mapdata', 'ggplot2', 'ggmap')
+packages <- c('maps', 'mapdata', 'ggplot2', 'ggmap', 'rjson', 'plyr')
 new_packages <- packages[!(packages %in% installed.packages()[,"Package"])]
 if (length(new_packages)) {
-  install.packages(new_packages, dependencies=TRUE)
+  install.packages(new_packages, dependencies=TRUE, repos='https://cran.ms.unimelb.edu.au/')
 }
 
 
@@ -11,6 +12,60 @@ library(maps)
 library(mapdata)
 library(ggplot2)
 library(ggmap)
+library(rjson)
+library(plyr)
+
+config <- fromJSON(file='config.json')
+register_google(key=config$google_api_key) 
+
+
+world <- map_data('world')
+
+visited <- read.csv('visited.csv')  # list of countries visited
+person_a <- as.vector(visited[visited$person=='PersonA', ]$country)
+person_b <- as.vector(visited[visited$person=='PersonB', ]$country)
+planned <- as.vector(visited[visited$person=='Planned', ]$country)
+
+
+# get flights data
+df <- read.csv('flights.csv')
+
+locations.fname <- 'c:/workspace/maps/locations.csv'
+if (file.exists(locations.fname)) {
+  locations.saved <- read.csv(file=locations.fname)
+} else {
+  locations.saved <- data.frame(city=c(), lat=c(), lon=c())
+}
+
+cities <- Reduce(union, list(df$start, df$end))
+
+# get missing locations
+locations.missing <- setdiff(cities, locations.saved$city)
+
+# get missing locations using google geocode api
+if (length(locations.missing > 0)) {
+  locations.new <- geocode(locations.missing)
+  locations.new$city <- locations.missing
+  print(locations.new)
+
+  locations.all <- rbind(locations.saved, locations.new)
+} else {
+  locations.all <- locations.saved
+}
+
+# save for next time
+write.csv(locations.all, "locations.csv", row.names=FALSE)
+
+
+apply_map <- function(df, from, to) {
+  return(as.numeric(as.character(mapvalues(df, from=from, to=to))))
+}
+
+# map cities lat/lon to flights dataframe
+df$start.lat <- apply_map(df$start, locations.all$city, locations.all$lat)
+df$start.lon <- apply_map(df$start, locations.all$city, locations.all$lon)
+df$end.lat <- apply_map(df$end, locations.all$city, locations.all$lat)
+df$end.lon <- apply_map(df$end, locations.all$city, locations.all$lon)
 
 
 add_country <- function(df, country, fill) {
@@ -31,49 +86,26 @@ add_countries <- function(p, df, countries, colour) {
 }
 
 
-world <- map_data('world')
-
-visited <- read.csv('visited.csv')  # list of countries visited
-person_a <- as.vector(visited[visited$person=='PersonA', ]$country)
-person_b <- as.vector(visited[visited$person=='PersonB', ]$country)
-planned <- as.vector(visited[visited$person=='Planned', ]$country)
-
-
 mp <- ggplot() + borders("world", colour="black", fill="white", size=0.5)
-
 # shade red/blue and jointly visited countries are purple (alpha is 0.5)
 mp <- add_countries(mp, world, person_a, 'red')
 mp <- add_countries(mp, world, person_b, 'blue')
-
 ggsave('output/plot.png', plot=mp, width=20, height=12)
-
-# mp <- add_countries(mp, world, planned, 'green')
-# ggsave('output/plot_with_planned.png', plot=mp, width=20, height=12)
-
-
-df <- read.csv('flights.csv')
-
-# TODO: save values, this is stupid.
-starts <- geocode(as.vector(df$start))
-ends <- geocode(as.vector(df$end))
 
 
 # add cities dots from route paths
-locations <- unique(rbind(starts, ends))
 mp <- mp + geom_point(
-  aes(x=locations$lon, y=locations$lat),
+  aes(x=locations.all$lon, y=locations.all$lat),
   color='green', size=1
 )
-
 ggsave('output/plot_with_cities.png', plot=mp, width=20, height=12)
 
 
 # add route paths (flights in this example) to map using curves between cities
 mp <- mp + geom_curve(
-  aes(x=starts$lon, y=starts$lat, xend=ends$lon, yend=ends$lat),
+  aes(x=df$start.lon, y=df$start.lat, xend=df$end.lon, yend=df$end.lat),
   size=0.25,
   color="magenta",
   curvature=0.1
 )
-
 ggsave('output/plot_with_cities_and_curve.png', plot=mp, width=20, height=12)
